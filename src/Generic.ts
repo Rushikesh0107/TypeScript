36aa637f277fb9a459028197d692615a2d0cb030
Rushikesh0107 <rushikeshdadas03@gmail.com>
2024-02-13 12:04:56 +0530

    generics

--- src/Generic.ts
+++ src/Generic.ts
@@ -6,10 +6,18 @@ function getFirstElement<T>(arr: T[]): T {
 
 //you can define the type while calling the function
 
+interface UserInGeneric {
+    name: string;
+    age: number;
+}
+
 let elm = getFirstElement<number>([1,2,3,4]) //type : number
 let elm1 = getFirstElement<string>(["a","b","c","d"])  //type: string
 let elm2 = getFirstElement<boolean>([true,false,true,false]) //type: boolean
+let userInGeneric = getFirstElement<UserInGeneric>([{name: "John", age: 25}]) //type: User
 
 console.log(typeof elm);
 console.log(typeof elm1);
 console.log(typeof elm2);
+console.log(typeof userInGeneric);
+
